97b59e4fec8328b63b99000187ae5e41fbc84d83
davidallenbaer <davidallenbaer@gmail.com>
2021-05-19 21:47:31 -0400

    Add project files.

---
+++ DesignPattern_ChainOfResponsibility/Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPattern_ChainOfResponsibility
+{
+    public abstract class Handler
+    {
+        public Handler rsHandler;
+        public void nextHandler(Handler rsHandler)
+        {
+            this.rsHandler = rsHandler;
+        }
+
+        public abstract void dispatchRs(long requestedAmount);
+    }
+}
---
+++ DesignPattern_ChainOfResponsibility/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_ChainOfResponsibility
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            Console.WriteLine("\nRequested Amount 5600");
+            atm.withdraw(5600);
+            Console.WriteLine("\nRequested Amount 2900");
+            atm.withdraw(2900);
+            Console.WriteLine("\nRequested Amount 700");
+            atm.withdraw(700);
+            Console.Read();
+        }
+    }
+}
